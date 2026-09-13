d77490ecb1a89ae523b213ad2358cbfe148856cb
neanthertal-albino <iagogeneral2@gmail.com>
2026-09-13 00:19:06 -0300

    inimigo

--- inimigo/inimigo.py
+++ inimigo/inimigo.py
@@ -5,6 +5,7 @@ class Inimigo(ABC):
     def __init__(self):
         self.nome = ''
         self._hp = 0
+        self.agilidade = 0
 
 
     def receber_dano(self, dano):
@@ -26,46 +27,15 @@ class Inimigo(ABC):
         return self._hp
 
 
-    def to_dict(self):
-        return {
-            'tipo': self.__class__.__name__,
-            'nome': self.nome,
-            'hp': self._hp
-        }
-
-
-    @classmethod
-    def from_dict(cls, data):
-        classe_concreta = Inimigo.CLASSES.get(data['tipo'])
-
-        if classe_concreta is None:
-            raise ValueError(f'Tipo de inimigo desconhecido ao carregar: "{data["tipo"]}"')
-
-        inimigo = classe_concreta()
-        inimigo.nome = data['nome']
-        inimigo._hp = data['hp']
-
-        return inimigo
-
-
 class Esqueleto(Inimigo):
     def __init__(self):
         super().__init__()
         self.nome = 'Esqueleto'
         self._hp = 30
+        self.agilidade = 3
 
     
     def atacar(self, alvo):
         dano = 10
         alvo.receber_dano(dano)
         return dano
-
-
-'''
-=================
-REGISTRO CLASSES
-=================
-'''
-Inimigo.CLASSES = {
-        'Esqueleto': Esqueleto
-    }
